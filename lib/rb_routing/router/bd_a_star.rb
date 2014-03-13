444410c90072e1b1f7cfe8bd364f026c5a07254f
Jordan Anderson <jordandrsn@gmail.com>
2014-03-12 23:30:29 -0400

    Bidirectional algorithms can subclass regular versions

--- lib/rb_routing/router/bd_a_star.rb
+++ lib/rb_routing/router/bd_a_star.rb
@@ -1,43 +1,10 @@
 module RbRouting
   module Router
 
-    class BdAStar < RbRouting::Base
-
-      def set_routing_params(options = {})
-        super
-
-        @x1_field ||= options[:x1] || "x1"
-        @x2_field ||= options[:x2] || "x2"
-        @y1_field ||= options[:y1] || "y1"
-        @y2_field ||= options[:y2] || "y2"
-      end
-
-      def routing_function_defaults
-        {
-          :sql                  => cost_query.sql,
-          :source               => :required,
-          :target               => :required,
-          :directed             => false,
-          :has_reverse_cost     => false
-        }
-      end
+    class BdAStar < RbRouting::Router::AStar
 
       def routing_function_definition
         {:pgr_bdastar => [:sql, :source, :target, :directed, :has_reverse_cost]}
-      end
-
-      def cost_query_select
-        {
-          'id'            => @id_field,
-          'source'        => @source_field,
-          'target'        => @target_field,
-          'x1'            => @x1_field,
-          'x2'            => @x2_field,
-          'y1'            => @y1_field,
-          'y2'            => @y2_field,
-          'cost'          => @cost_field,
-          'reverse_cost'  => @reverse_cost_field 
-        }
       end
 
     end
--- lib/rb_routing/router/bd_dijkstra.rb
+++ lib/rb_routing/router/bd_dijkstra.rb
@@ -1,17 +1,7 @@
 module RbRouting
   module Router
 
-    class BdDijkstra < RbRouting::Base
-
-      def routing_function_defaults
-        {
-          :sql                  => cost_query.sql,
-          :source               => :required,
-          :target               => :required,
-          :directed             => false,
-          :has_reverse_cost     => false
-        }
-      end
+    class BdDijkstra < RbRouting::Router::Dijkstra
 
       def routing_function_definition
         {:pgr_bdDijkstra => [:sql, :source, :target, :directed, :has_reverse_cost]}
