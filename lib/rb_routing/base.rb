5cc81644e7017ad4e006ff6dac14b3f1c085897f
Jordan Anderson <jordandrsn@gmail.com>
2014-03-20 07:20:39 -0400

    More docs

--- lib/rb_routing/base.rb
+++ lib/rb_routing/base.rb
@@ -23,13 +23,15 @@ module RbRouting #:nodoc:
   #
   # All of pgRouting's shortest path functions return their results using a custom
   # postgres data type called <tt>pgr_costResult</tt>: 
-  # <tt>seq</tt> - the step number of the path
-  # <tt>id1</tt> - typically the node id 
-  # <tt>id2</tt> - typically the edge id
-  # <tt>cost</tt> - the cost incurred at that step
+  # - <tt>seq</tt> - the step number of the path
+  # - <tt>id1</tt> - typically the node id 
+  # - <tt>id2</tt> - typically the edge id
+  # - <tt>cost</tt> - the cost incurred at that step
+  #
+  # By default, 
   class Base 
 
-    def set_db_params(options = {})
+    def set_db_params(options = {}) #:nodoc:
       @db           ||= options[:database]
       @host         ||= options[:host]          || "localhost"
       @user         ||= options[:user] 
@@ -42,7 +44,7 @@ module RbRouting #:nodoc:
       connect
     end
 
-    def set_routing_params(options = {})
+    def set_routing_params(options = {}) #:nodoc:
       @id_field           ||= options[:id]              || :gid
       @source_field       ||= options[:source]          || :source
       @target_field       ||= options[:target]          || :target
@@ -51,38 +53,47 @@ module RbRouting #:nodoc:
       @edge_table         ||= options[:edge_table]      || :ways
     end
 
-    def set_import_params(options = {})
+    def set_import_params(options = {}) #:nodoc:
       @clean_tables   ||= options[:clean]       || true
       @skip_nodes     ||= options[:skip_nodes]  || true
       @import_options = {:clean_tables => @clean_tables, :skip_nodes => @skip_nodes}
     end
 
+    # Specify a new instance of RbRouting::Base, including any DB params, osm2pgrouting import params, 
+    # and routing params
     def initialize(options={})
       set_db_params(options)
       set_import_params(options)
       set_routing_params(options)
     end
 
+    # A connection to the postgres database
     def connection 
       @connection.pg rescue nil
     end
 
+    # Connect to the postgres database
     def connect
       @connection ||= ::RbRouting::Connection.new(@db_options)
     end
 
+    # Display an array of validation or routing errors that have been raised since the last run
     def errors
       @errors
     end
 
+    # The raw postgres routing function result set as an array
     def result 
       @result
     end
 
+    # An instance of RbRouting::Path (or whatever has been specified in #path_class) containing 
+    # the shortest path from the last run
     def path
       @path
     end
 
+    # The class to use for the shortest path results. Default is RbRouting::Path
     def path_class
       RbRouting::Path
     end
@@ -95,6 +106,14 @@ module RbRouting #:nodoc:
       "define in subclass"
     end
 
+    # A hash specifying the select clause of the cost query. By default it is:
+    #   {
+    #     'id'            => @id_field,
+    #     'source'        => @source_field,
+    #     'target'        => @target_field,
+    #     'cost'          => @cost_field,
+    #     'reverse_cost'  => @reverse_cost_field 
+    #   }
     def cost_query_select
       {
         'id'            => @id_field,
@@ -109,10 +128,19 @@ module RbRouting #:nodoc:
       @edge_table
     end
 
+    # The where clause of the cost query
     def cost_query_where
       ""
     end
 
+    # A hash specifying the select clause of the results query. By default it is:
+    #   {
+    #     'seq'           => :seq,
+    #     'node'          => :id1,
+    #     'edge'          => :id2,
+    #     'cost'          => :cost,
+    #     'reverse_cost'  => :cost
+    #   }
     def results_query_select
       {
         'seq'           => :seq,
@@ -123,33 +151,34 @@ module RbRouting #:nodoc:
       }
     end
 
-    def results_query_from
+    def results_query_from #:nodoc:
       Sequel.function(routing_function, *routing_function_params)
     end
 
+    # The where clause of the results query
     def results_query_where
       ""
     end
 
-    def cost_query
+    def cost_query #:nodoc:
       select_args = cost_query_select.map {|k,v| Sequel.as(v, k) }
       connection.select(*select_args).from(cost_query_from).where(cost_query_where)
     end
 
-    def results_query
+    def results_query #:nodoc:
       select_args = results_query_select.map {|k,v| Sequel.as(v, k) }
       connection.select(*select_args).from(*results_query_from).where(results_query_where)
     end
 
-    def routing_function 
+    def routing_function #:nodoc:
       routing_function_definition.keys.first
     end
 
-    def routing_function_params
+    def routing_function_params #:nodoc:
       routing_function_definition.values.flatten.map {|v| @run_options[v] }.reject {|p| p.nil? }
     end
 
-    def set_defaults(options = {})
+    def set_defaults(options = {}) #:nodoc:
       routing_function_defaults.each do |key, value|
         options[key] = value if options[key].blank? and ![:optional, :required].include?(value) 
       end
@@ -157,7 +186,7 @@ module RbRouting #:nodoc:
       options
     end
 
-    def validations(options = {})
+    def validations(options = {}) #:nodoc:
       @errors = []
       routing_function_defaults.each do |key, value|
         @errors << "'#{key}'" if options[key].blank? and value == :required
@@ -168,10 +197,17 @@ module RbRouting #:nodoc:
       @errors
     end
 
+    # Import data into the specified edge_table using osm2pgrouting
+    #
+    #   router = RbRouting::Router::TrspByVertex.new :user => "postgres", :password => "postgres", :database => "routing"
+    #   router.import_osm_data "/path/to/san_francisco_osm_file.osm", "/path/to/my/pgrouting/mapconfig.xml"
     def import_osm_data(file_name, config_file, options={})
       ::RbRouting::import_osm_data(file_name, @db, @user, config_file, options.merge(@db_options).merge(@import_options))
     end
 
+    # Find the shortest path via the cost and results queries built along with any parameters provided 
+    #   router = RbRouting::Router::TrspByVertex.new :user => "postgres", :password => "postgres", :database => "routing"
+    #   router.run :source => 550, :target => 5463
     def run(options = {})
       options = set_defaults(options)
       validations(options)
