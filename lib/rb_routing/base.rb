450e5ccda37dcf7f439e9b98f16dfcb6c63f4505
Jordan Anderson <jordandrsn@gmail.com>
2014-03-23 23:10:04 -0400

    Migrate docs to markdown

--- lib/rb_routing/base.rb
+++ lib/rb_routing/base.rb
@@ -11,16 +11,16 @@ module RbRouting
   # == Cost query
   # A query that should return a particular set of columns that define key inputs 
   # into the routing function. For example, the Dijkstra algorithm requires:
-  # <tt>SELECT id, source, target, cost [,reverse_cost] FROM edge_table </tt> where
+  # `SELECT id, source, target, cost [,reverse_cost] FROM edge_table` where
   # the reverse_cost column is optional. 
   #
   # This defines the graph that will be analyzed by the shortest path function and 
   # the columns (or expressions) used to calculate the cost of traversing each edge.
   #
   # The cost query is defined by 3 methods:
-  # - <tt>cost_query_select</tt> - returns a has containing the fields to select and their aliases
-  # - <tt>cost_query_from</tt> - defaults to the <tt>edge_table</tt>
-  # - <tt>cost_query_where</tt> - defaults to a blank string. This would allow you to narrow down the
+  # - `cost_query_select` - returns a has containing the fields to select and their aliases
+  # - `cost_query_from` - defaults to the `edge_table`
+  # - `cost_query_where` - defaults to a blank string. This would allow you to narrow down the
   #   network the shortest path function is searching.
   #
   # == Results query
@@ -29,11 +29,11 @@ module RbRouting
   # or view to enrich the data (e.g. street names, OSM tags, etc.).
   #
   # All of pgRouting's shortest path functions return their results using a custom
-  # postgres data type called <tt>pgr_costResult</tt>: 
-  # - <tt>seq</tt> - the step number of the path
-  # - <tt>id1</tt> - typically the node id 
-  # - <tt>id2</tt> - typically the edge id
-  # - <tt>cost</tt> - the cost incurred at that step
+  # postgres data type called `pgr_costResult`: 
+  # - `seq` - the step number of the path
+  # - `id1` - typically the node id 
+  # - `id2` - typically the edge id
+  # - `cost` - the cost incurred at that step
   class Base 
 
     def set_db_params(options = {})
@@ -64,7 +64,7 @@ module RbRouting
       @import_options = {:clean_tables => @clean_tables, :skip_nodes => @skip_nodes}
     end
 
-    # Specify a new instance of RbRouting::Base, including any DB params, osm2pgrouting import params, 
+    # Specify a new instance of {RbRouting::Base}, including any DB params, osm2pgrouting import params, 
     # and routing params
     def initialize(options={})
       set_db_params(options)
@@ -72,33 +72,33 @@ module RbRouting
       set_routing_params(options)
     end
 
-    # A connection to the postgres database
+    # A connection to the postgres database.
     def connection 
       @connection.pg rescue nil
     end
 
-    # Connect to the postgres database
+    # Connect to the postgres database.
     def connect
       @connection ||= ::RbRouting::Connection.new(@db_options)
     end
 
-    # Display an array of validation or routing errors that have been raised since the last run
+    # Display an array of validation or routing errors that have been raised since the last run.
     def errors
       @errors
     end
 
-    # The raw postgres routing function result set as an array
+    # The raw postgres routing function result set as an array.
     def result 
       @result
     end
 
-    # An instance of RbRouting::Path (or whatever has been specified in #path_class) containing 
-    # the shortest path from the last run
+    # An instance of {RbRouting::Path} (or whatever has been specified in {#path_class}) containing 
+    # the shortest path from the last run.
     def path
       @path
     end
 
-    # The class to use for the shortest path results. Default is RbRouting::Path
+    # The class to use for the shortest path results. Default is {RbRouting::Path}.
     def path_class
       RbRouting::Path
     end
@@ -129,11 +129,12 @@ module RbRouting
       }
     end
 
+    # The from clause of the cost query. Defaults to the edge_table.
     def cost_query_from
       @edge_table
     end
 
-    # The where clause of the cost query
+    # The where clause of the cost query. Defaults to an empty string.
     def cost_query_where
       ""
     end
@@ -156,11 +157,14 @@ module RbRouting
       }
     end
 
+    # The from clause of the results query. Defaults to the shortest path function
+    # defined by `routing_function` with the params defined in 
+    # `routing_function_params`.
     def results_query_from
       Sequel.function(routing_function, *routing_function_params)
     end
 
-    # The where clause of the results query
+    # The where clause of the results query.
     def results_query_where
       ""
     end
@@ -183,6 +187,9 @@ module RbRouting
       routing_function_definition.values.flatten.map {|v| @run_options[v] }.reject {|p| p.nil? }
     end
 
+    # Each shortest path function includes a set of defaults, defined in {#routing_function_defaults}.
+    # This method sets defaults for any routing function parameters that have not been explicitly
+    # defined when calling {#run}.
     def set_defaults(options = {})
       routing_function_defaults.each do |key, value|
         options[key] = value if options[key].blank? and ![:optional, :required].include?(value) 
@@ -202,17 +209,21 @@ module RbRouting
       @errors
     end
 
-    # Import data into the specified edge_table using osm2pgrouting
+    # Import data into the specified edge_table using osm2pgrouting.
     #
-    #   router = RbRouting::Router::TrspByVertex.new :user => "postgres", :password => "postgres", :database => "routing"
-    #   router.import_osm_data "/path/to/san_francisco_osm_file.osm", "/path/to/my/pgrouting/mapconfig.xml"
+    # Example:<br />
+    # `router = RbRouting::Router::TrspByVertex.new :user => "postgres", :password => "postgres", :database => "routing"`<br />
+    # `router.import_osm_data "/path/to/san_francisco_osm_file.osm", "/path/to/my/pgrouting/mapconfig.xml"`
     def import_osm_data(file_name, config_file, options={})
       ::RbRouting::import_osm_data(file_name, @db, @user, config_file, options.merge(@db_options).merge(@import_options))
     end
 
-    # Find the shortest path via the cost and results queries built along with any parameters provided 
-    #   router = RbRouting::Router::TrspByVertex.new :user => "postgres", :password => "postgres", :database => "routing"
-    #   router.run :source => 550, :target => 5463
+    # Find the shortest path via the cost and results queries built along with any parameters provided.
+    #
+    #
+    # Example:<br />
+    # `router = RbRouting::Router::TrspByVertex.new :user => "postgres", :password => "postgres", :database => "routing"`<br />
+    # `router.run :source => 550, :target => 5463`
     def run(options = {})
       options = set_defaults(options)
       validations(options)
